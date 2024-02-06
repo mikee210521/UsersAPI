57fa2771764fde9d6f74d5209303aef7c195e6e2
mike21021 <angel210521@gmail.com>
2024-02-06 16:34:03 -0600

    api users random

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+      <title>Technical Test</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
